a986a047f897f41f6cbe8e22196aa6d2d4158707
willivmx <jadannouzonon@gmail.com>
2024-08-01 12:11:07 +0100

    prettify codebase with prettier

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -172,9 +172,7 @@ const Burger = () => {
                 );
               })}
             </div>
-            <div
-              className={"flex justify-center items-center w-full"}
-            >
+            <div className={"flex justify-center items-center w-full"}>
               <Buttons />
             </div>
           </div>
